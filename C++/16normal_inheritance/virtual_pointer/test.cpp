7916c59f9d1de58a61e53630873571b87cce96ea
Zhao Jiarong <kawing.chiu.sysu@gmail.com>
2015-10-22 12:54:29 +0800

    Add virtual pointer test

---
+++ C++/16normal_inheritance/virtual_pointer/test.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+
+class base {
+    public:
+        virtual void read() = 0;
+        virtual void write() {
+            cout << "in base's write" << endl;
+        };
+        virtual ~base() {};
+    private:
+        int _i;
+};
+
+class transmitter : public base {
+    public:
+        virtual void write() override {
+            cout << "in transmitter's write" << endl;
+            read();
+        }
+        virtual void read() override {
+            cout << "in transmitter's read" << endl;
+        }
+};      
+
+
+class radio : public transmitter {
+    public:
+        virtual void read() override {
+            cout << "in radio's read" << endl;
+        }
+        virtual void test() {
+            cout << "in radio's test" << endl;
+        }
+};
+
+void print_spr() {
+    cout << "-------------" << endl;
+}
+
+int main() {
+    radio *r = new radio();
+
+    r->read();
+    r->transmitter::read();
+
+    //print_spr();
+
+    //transmitter *t = new radio();
+    //t->read();
+
+    print_spr();
+
+    base *b = new radio();
+    // radio's read:
+    b->read();
+    // still radio's read:
+    dynamic_cast<transmitter *>(b)->read();
+    // finally, transmitter's read:
+    dynamic_cast<transmitter *>(b)->transmitter::read();
+    dynamic_cast<radio *>(b)->test();
+
+}

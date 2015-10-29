2b51a31adfec1dd054051a129be7f2acd555cbd1
Zhao Jiarong <kawing.chiu.sysu@gmail.com>
2015-10-29 11:39:30 +0800

    Add vector test.

--- C++/02stdlib/string/test.cpp
+++ C++/02stdlib/string/test.cpp
@@ -25,6 +25,14 @@ int main() {
     string str2 = "This is a long string.";
     cout << "str2: " << str2 << endl;
     auto n = str2.find("long");
-    cout << n << ": " << str2.substr(n) << endl;
+    cout << "finding 'long': " << n << ", " << str2.substr(n) << endl;
+
+    // convert number(or other basic types) to string:
+    string num = std::to_string(30);
+    cout << num[0] << endl;
+    // the C way:
+    char num2[10];
+    sprintf(num2, "%d", 50);
+    cout << num2 << endl;
 
 }
---
+++ C++/11stl/vector/test.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+    vector<int> v;
+    v.push_back(3);
+    v.push_back(10);
+    v.push_back(33);
+
+    for (size_t i = 0; i < v.size(); i++) {
+        cout << "v[" << i << "]: " << v[i] << endl;
+    }
+
+    if (!v.empty()) {
+        v.clear();
+    }
+
+    vector<int> vv;
+    vv.push_back(10);
+    v.push_back(10);
+
+    if (v == vv) {
+        cout << "the same!" << endl;
+    }
+
+    v.push_back(20);
+    v.push_back(30);
+
+    using iter_type = std::vector<int>::iterator;
+    for (iter_type it = v.begin(); it != v.end(); it++) {
+        cout << *it << endl;
+    }
+
+    for (auto& it : v) {
+        it += 1;
+    }
+
+    // using for_each
+    std::for_each(v.begin(), v.end(), [](int &elem) {
+        elem += 2;
+    });
+
+    // WRONG!!!
+    // without reference, cannot get reference:
+    //for (auto it : v) {
+    //    auto i  = &it - &v[0];
+    //    cout << "v[" << i << "]: " << it << endl;
+    //}
+    for (auto& it : v) {
+        auto i  = &it - &v[0];
+        cout << "v[" << i << "]: " << it << endl;
+    }
+
+
+}

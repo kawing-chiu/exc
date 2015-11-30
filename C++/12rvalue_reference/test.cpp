45a37cbb40aee1d41b587b7340311b60f33fdd8a
Zhao Jiarong <kawing.chiu.sysu@gmail.com>
2015-11-30 20:11:52 +0800

    Fix rvalue test.

--- C++/12rvalue_reference/test.cpp
+++ C++/12rvalue_reference/test.cpp
@@ -14,12 +14,15 @@ void f(string&& s) {
 
 int main() {
     // not working:
-    //string&& s = get_str();
+    //string& s = get_str();
     string&& s = get_str();
     s[2] = 'x';
     cout << s << endl;
 
-    // no matching call:
+    // no matching call
+    // s is already a lvalue
     //f(s);
     f("ttt");
+    // after std::move(), usually s should not be used anymore
+    f(std::move(s));
 }

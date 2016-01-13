0c84bdf27959465990987f1a58970a58e95a691b
Zhao Jiarong <kawing.chiu.sysu@gmail.com>
2016-01-14 07:00:32 +0800

    Pandas.

--- Python/15pandas/tutorial/10-min-tor.py
+++ Python/15pandas/tutorial/10-min-tor.py
@@ -105,6 +105,26 @@ df2[df2['E'].isin(['two','four'])]
 ### 4. merging
 print("\n### 4. merging")
 
+df = pd.DataFrame(np.random.randn(10, 4))
+
+# basic concat (combining columns)
+pieces = [df[:3], df[3:7], df[7:]]
+pd.concat(pieces)
+
+# database like joining
+left = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3'],
+                     'A': ['A0', 'A1', 'A2', 'A3'],
+                     'B': ['B0', 'B1', 'B2', 'B3']})
+right = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3'],
+                      'C': ['C0', 'C1', 'C2', 'C3'],
+                      'D': ['D0', 'D1', 'D2', 'D3']})
+joined = pd.merge(left, right, on='key')
+
+# append rows
+df = pd.DataFrame(np.random.randn(8, 4), columns=['A','B','C','D'])
+s = df.iloc[3]
+# note the use of ignore_index
+df.append(s, ignore_index=True)
 
 ### 5. grouping
 print("\n### 5. grouping")

9ba4a7d8b754a41aabada95419d87b564650dd13
Zhao Jiarong <kawing.chiu.sysu@gmail.com>
2017-11-01 14:05:23 +0800

    Minor changes.

--- Python/22method_registry/test.py
+++ Python/22method_registry/test.py
@@ -9,9 +9,9 @@ class MyClass:
     register = Registry()
 
     @register("tag1", "tag2")
-    def my_method( arg1,arg2 ):
+    def my_method(arg1, arg2):
         pass
 
     @register("tag3")
-    def my_other_method( arg1,arg2 ):
+    def my_other_method(arg1, arg2):
         pass

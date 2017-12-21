fc137cfb6454492202b08819ad278d6ac582ea98
Zhao Jiarong <kawing.chiu.sysu@gmail.com>
2017-12-21 14:41:29 +0800

    Add tensorflow test.

---
+++ Python/24tensorflow/linear_model/linear_model_tutorial.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+
+
+# use cmd `tensorboard --logdir <log_dir>` to show log
+log_dir = '/tmp/test/linear_model'
+
+# Model parameters
+W = tf.Variable([.3], dtype=tf.float32, name='weight')
+b = tf.Variable([-.3], dtype=tf.float32, name='offset')
+# Model input and output
+x = tf.placeholder(tf.float32, name='x')
+linear_model = W*x + b
+y = tf.placeholder(tf.float32, name='y')
+
+# loss
+loss = tf.reduce_sum(tf.square(linear_model - y)) # sum of the squares
+# optimizer
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+
+# training data
+x_train = [1, 2, 3, 4]
+y_train = [0, -1, -2, -3]
+# training loop
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    writer = tf.summary.FileWriter(log_dir, sess.graph)
+    sess.run(init) # reset values to wrong
+    for i in range(1000):
+      sess.run(train, {x: x_train, y: y_train})
+    
+    # evaluate training accuracy
+    curr_W, curr_b, curr_loss = sess.run(
+        [W, b, loss], {x: x_train, y: y_train})
+    print("W: %s b: %s loss: %s"%(curr_W, curr_b, curr_loss))
+
+

023c5050f2959886fb10629f24b956f6c976388a
Zhao Jiarong <kawing.chiu.sysu@gmail.com>
2018-09-05 19:07:09 +0800

    Update coroutine test.

--- Python/30coroutine/basic_test.py
+++ Python/30coroutine/basic_test.py
@@ -1,4 +1,4 @@
-
+# Native coroutine test
 class MyAwaitable:
     def __await__(self):
         y = yield 10
@@ -22,6 +22,28 @@ async def coro():
     print("awaited 2:", b)
     return "my return value"
                              
+# Generator-based coroutine test
+def my_generator():
+    y = yield 10
+    print("yielded:", y)
+    y = yield 'a'
+    print("yielded:", y)
+    return "end of generator 1"
+
+def my_generator2():
+    y = yield 20
+    print("yielded:", y)
+    y = yield 'b'
+    print("yielded:", y)
+    return "end of generator 2"
+                             
+def gen():              
+    a = yield from my_generator()
+    print("yielded from:", a)
+    b = yield from my_generator2()
+    print("yielded from 2:", b)
+    return "my return value"
+
                              
 if __name__ == "__main__":   
     c = coro()              
